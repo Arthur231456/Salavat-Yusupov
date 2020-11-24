ee7b6562eae0eb69d6a5e4b59f7e0f885f046b37
Arthur231456 <artur.231456@gmail.com>
2020-11-24 23:18:52 +0500

    exercise 3

--- circle.py
+++ circle.py
@@ -3,13 +3,41 @@ from PyQt5 import QtCore, QtGui, uic
 from random import choice as c
 from PyQt5.QtGui import QColor
 import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Drawer(QW.QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.btn = QtWidgets.QPushButton(self.centralwidget)
+        self.btn.setGeometry(QtCore.QRect(690, 510, 101, 31))
+        self.btn.setObjectName("btn")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Желтые круги"))
+        self.btn.setText(_translate("MainWindow", "Рисовать"))
+
+
+class Drawer(QW.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.paint = False
-        uic.loadUi('form.ui', self)
+        self.setupUi(self)
         self.btn.clicked.connect(self.draw)
 
     def draw(self):
